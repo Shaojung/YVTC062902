5eb35abac0ed56aee62879be4678c8538ee6c291
Shaojung <shaojung@gmail.com>
2017-06-29 10:57:55 +0800

    Button setOnClickListener

--- app/src/main/java/com/example/yvtc/yvtc062902/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062902/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yvtc.yvtc062902;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        MyClick cc = new MyClick();
+        btn.setOnClickListener(cc);
+    }
+    class MyClick implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            TextView tv = (TextView) findViewById(R.id.textView3);
+            tv.setText("Hello World");
+        }
     }
 }
+
